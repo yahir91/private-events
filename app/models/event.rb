2d4c930dbaf1c9c61af2b943b75fb3f4fc1b83ea
maosan132 <maosan132@gmail.com>
2020-08-18 17:03:04 -0500

    fixed errors in models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
     has_many :attendances, foreign_key: :event_attended_id
-    has_many :attendees through: :attendance, source: :attendee
+    has_many :attendees, through: :attendance, source: :attendee
     belongs_to :creator, class_name: "User"
 end
