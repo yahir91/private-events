47bcd866793fc466301518f7581b4c4077e0722d
maosan132 <maosan132@gmail.com>
2020-08-25 17:27:42 -0500

    started with changes to make signup link disappear as soon user is loggedin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
-
+  def log_in(user)
+    session[:user_id] = user.id
+  end
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
     else
       @current_user = nil
     end
+  end
+  def logged_in?
+    !current_user.nil?
   end
 end
