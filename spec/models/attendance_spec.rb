60f2c6335fe9873b3e5982a9902616b77f74205f
maosan132 <maosan132@gmail.com>
2020-08-26 11:15:31 -0500

    updated css

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -6,6 +6,3 @@ RSpec.describe Attendance, type: :model do
   it { should belong_to(:attendee) }
 
   end
-
-  # belongs_to :attendee, class_name: 'User'
-  # belongs_to :event_attended, class_name: 'Event'
